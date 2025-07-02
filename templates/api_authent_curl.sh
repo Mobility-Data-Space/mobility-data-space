f3420e7fa1043cd48a6c7b38d78faf4c32aec911
Igor Petrov <91048868+ip312@users.noreply.github.com>
2025-07-02 15:53:37 +0200

    Delete api_authent_curl.sh

--- templates/api_authent_curl.sh
+++ templates/api_authent_curl.sh
@@ -1,5 +1,1 @@
-curl \
--d 'grant_type=client_credentials' \
--d 'client_id=[Please set your client id]' \
--d 'client_secret=[Please set your client secret]' \
-'[Please set the access token url]'
+
